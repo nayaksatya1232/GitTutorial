17137c5e31d8761cd518889c78407f08b47e34bb
nayaksatya1232 <nayaksatya1232@gmail.com>
2022-12-06 21:31:54 +0530

    Folder Added

---
+++ Folder01/first.java
@@ -0,0 +1,5 @@
+import System.lang.*
+public class first{}[D
+ public static void main(){}[D
+[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C  [B[C[C[C[[A[C[C[C[C[C[C[C[[A[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[CIts a java File
