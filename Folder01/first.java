d400ff2af45b2f68482f626457b51d90567ea1bd
nayaksatya1232 <nayaksatya1232@gmail.com>
2022-12-06 21:39:45 +0530

    test

--- Folder01/first.java
+++
@@ -1,5 +0,0 @@
-import System.lang.*
-public class first{}[D
- public static void main(){}[D
-[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C  [B[C[C[C[[A[C[C[C[C[C[C[C[[A[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[CIts a java File
